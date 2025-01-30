4d1e1276dedafc29198d2c7de4eafaf7dbb4f26c
Pink727 <doc72789@gmail.com>
2025-01-30 14:33:42 -0800

    Add end-to-end test for Tech Quiz functionality

---
+++ cypress/e2e/e2equiz.cy.ts
@@ -0,0 +1,17 @@
+describe('Tech Quiz', () => {
+    it('should start the quiz and complete it', () => {
+      cy.visit('/');
+      cy.get('button').contains('Start Quiz').click();
+      cy.get('h2').should('be.visible');
+  
+      for (let i = 0; i < 10; i++) {
+        // Check that there are 4 answer buttons
+        cy.get('button.btn.btn-primary').should('have.length', 4);
+        // Click the first answer button
+        cy.get('button.btn.btn-primary').first().click();
+      }
+  
+      cy.get('h2').contains('Quiz Completed').should('be.visible');
+      cy.get('div.alert-success').should('be.visible');
+    });
+  });

034e90c014a2c59cb645bfd9b37c949aaeb5b683
Pink727 <doc72789@gmail.com>
2025-01-30 14:34:38 -0800

    Refactor seed script to read question data from JSON file using fs and path modules

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,8 +1,14 @@
 import db from '../config/connection.js';
-import { Question } from '../models/index.js'
+import { Question } from '../models/index.js';
 import cleanDB from './cleanDb.js';
+import fs from 'fs';
+import path from 'path';
 
-import questionData from './pythonQuestions.json' assert{ type: 'json'};
+// Resolve the directory path
+const __dirname = path.resolve();
+const questionDataPath = path.join(__dirname, './src/seeds/pythonQuestions.json');
+const questionData = JSON.parse(fs.readFileSync(questionDataPath, 'utf8'));
+
 
 try {
   await db();

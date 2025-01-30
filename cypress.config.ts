fd39801b690b8f2ea72f0b441dc56c7e886fb93c
Pink727 <doc72789@gmail.com>
2025-01-30 14:32:12 -0800

    Enhance Cypress configuration with component and e2e support files and spec patterns

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,15 +2,20 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
+    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
     devServer: {
       framework: 'react',
       bundler: 'vite',
     },
+    supportFile: 'cypress/support/component.ts',
+    indexHtmlFile: 'cypress/support/component-index.html',
   },
   e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://localhost:3001',
+    supportFile: 'cypress/support/e2e.ts',
   },
 });
